d0d98302c4ade0f71780abd8a493917c1a186292
tehtko <moderndsnsam@gmail.com>
2022-07-07 00:36:19 -0400

    Sandbox

--- Program.cs
+++ Program.cs
@@ -7,25 +7,8 @@ namespace sharp_nes
     {
         static void Main()
         {
-            C6502 CPU = new C6502(1);
-            Console.WriteLine(CPU.call(5));      
-            
-            Run();          
-        }
-        
-        public static void Run()
-        {
-            Console.WriteLine("Hit Escape to exit");
-        
-            do 
-	        {
-    		    while (! Console.KeyAvailable) 
-		        {
-        		    Console.WriteLine("Working");
-                    Thread.Sleep(1000);
-   	            }             
-	        } 
-            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
-        }
-    }
+            C6502 CPU = new C6502();
+            CPU.DoStuff();                                 
+        } 
+    }       
 }

397f18c9857478d320409a5a153833d3e26ee19e
tehtko <moderndsnsam@gmail.com>
2022-07-07 20:17:45 -0400

    Attach CPU to Bus

--- src/CPU/Bus.cs
+++ src/CPU/Bus.cs
@@ -2,6 +2,13 @@ namespace sharp_nes.CPU
 {
 	class Bus
 	{
+		private C6502 CPU { get; set; }
+		
+		public Bus()
+		{
+			CPU = new();
+		}
+		
 		public void write(ushort addr, byte data)
 		{
 			

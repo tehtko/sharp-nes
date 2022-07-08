5ce3bc8e8189c3343235a19351d2d9a2999173d7
tehtko <moderndsnsam@gmail.com>
2022-07-07 20:27:27 -0400

    Create RAM and clear on construct

--- src/CPU/Bus.cs
+++ src/CPU/Bus.cs
@@ -3,10 +3,12 @@ namespace sharp_nes.CPU
 	class Bus
 	{
 		private C6502 CPU { get; set; }
+		private byte[] RAM = new byte[64 * 1024];
 		
 		public Bus()
 		{
 			CPU = new();
+			for(int i=0;i<RAM.Length;i++) { RAM[i]=0x00; }
 		}
 		
 		public void write(ushort addr, byte data)

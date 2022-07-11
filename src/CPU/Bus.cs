d65f23380eadbd7db3bc7e2c5536df184b2f176c
tehtko <moderndsnsam@gmail.com>
2022-07-10 21:01:00 -0400

    Update Bus

--- src/CPU/Bus.cs
+++ src/CPU/Bus.cs
@@ -1,23 +1,23 @@
-namespace sharp_nes.CPU 
+namespace sharp_nes.CPU
 {
-	class Bus
+	public class Bus
 	{
 		private byte[] RAM = new byte[64 * 1024];
-		
+	
 		public Bus()
 		{
 			for(int i=0;i<RAM.Length;i++) { RAM[i]=0x00; }
 		}
-		
+	
 		public void write(ushort addr, byte data)
 		{
-			if(addr>=0x0000 && addr<= 0xFFFF)
+			if(addr>=0x000 && addr<=0xFFFF)
 				RAM[addr] = data;
 		}
 		
 		public byte read(ushort addr)
 		{
-			if(addr>=0x0000 && addr<=0xFFFF)
+			if(addr>=0x000 && addr<=0xFFFF)
 				return RAM[addr];
 				
 			return 0x00;

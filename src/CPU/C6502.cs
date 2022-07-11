364cf06261ca0f4bdcab039e4ad0b00f10ac4357
tehtko <moderndsnsam@gmail.com>
2022-07-10 21:01:10 -0400

    Update C6502

--- src/CPU/C6502.cs
+++ src/CPU/C6502.cs
@@ -2,19 +2,32 @@ namespace sharp_nes.CPU
 {
 	public class C6502
 	{
-		private Bus Bus { get; set; }
-		
 		private byte A = 0;
 		private byte X = 0;
 		private byte Y = 0;
+		
+		private Bus Bus { get; set; }
 		
 		public C6502()
 		{
 			Bus = new();
 		}
 		
-		void read(ushort addr) { Bus.read(addr); }
+		[Flags]
+		enum FLAGS
+		{
+			C = (1 << 0), // Carry Bit 
+			Z = (1 << 1), // Zero
+			I = (1 << 2), // Disable Interrupts
+			D = (1 << 3), // Decimal Mode
+			B = (1 << 4), // Break
+			U = (1 << 5), // Unused
+			V = (1 << 6), // Overflow
+			N = (1 << 7)  // Negative
+		};
 		
 		void write(ushort addr, byte data) { Bus.write(addr, data); }
-	}	
+		
+		void read(ushort addr) { Bus.read(addr); }
+	}
 }
